e4c32df3b34bbdec2d25b84a5cc44318420e0e49
castle <castleliu40@gmail.com>
2017-12-14 15:43:06 +0800

    update travis

--- factory.js
+++ factory.js
@@ -6,6 +6,14 @@ var snakeComponent = Qt.createComponent("Snake.qml");
 var wallComponent = Qt.createComponent("Wall.qml");
 var appleComponent = Qt.createComponent("Apple.qml");
 
-function func() {
+function GameFactory() {
+    this.board = null;
+    this.parentEntity = null;
+    this.entities = [];
+}
 
+GameFactory.prototype.removeAllEntities = function(){
+    for(var i = 0;i< this.entities.length;i++){
+        this.entities[i].setParent(null);
+    }
 }
